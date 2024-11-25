381af778c8aed00c5d5bc4a5743433be9061e2b5
hiddenSharp429 <z404878860@163.com>
2024-11-25 21:51:45 +0800

    🐛 Fix Config Bug

---
+++ config/config.example.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "your-api-key-here",
+    authDomain: "your-auth-domain-here",
+    databaseURL: "your-database-url-here",
+    projectId: "your-project-id-here",
+    storageBucket: "your-storage-bucket-here",
+    messagingSenderId: "your-messaging-sender-id-here",
+    appId: "your-app-id-here"
+};
+
+export default config; 
